c5ec5c405a031c47f858aeca92aacad75e22bb7e
Burak Güzey <burakguzey20@gmail.com>
2023-04-09 17:55:24 +0300

    Clima App is finished

---
+++ Clima App/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Clima App
+//
+//  Created by Burak Güzey on 9.04.2023.
+//
+
+import Foundation
+
+struct NetworkConstants {
+    
+    static let baseURL = "https://api.openweathermap.org/data/2.5/weather?appid=d58a02dec06c7be484264fc7562287e6&units=metric"
+
+}
